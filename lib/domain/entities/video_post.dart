191ab2f94badabc9cf6260298384dab98d59dfd4
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-07-09 17:46:36 -0500

    [add] model, post, fakeData

--- lib/domain/entities/video_post.dart
+++ lib/domain/entities/video_post.dart
@@ -2,11 +2,11 @@ class VideoPost {
   final String caption;
   final String videoUrl;
   final int likes;
-  final int view;
+  final int views;
 
   VideoPost(
       {required this.caption,
       required this.videoUrl,
       this.likes = 0,
-      this.view = 0});
+      this.views = 0});
 }
