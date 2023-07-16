da678b122ca5d271d3d3fbbada94737785e822b5
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-07-16 17:43:56 -0500

    [add] social media >>buttons

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableScreen extends StatelessWidget {
   final List<VideoPost> videos;
@@ -9,7 +10,25 @@ class VideoScrollableScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
+      scrollDirection: Axis.vertical,
+      physics: const BouncingScrollPhysics(),
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            Positioned(
+                bottom: 40,
+                right: 20,
+                child: VideoButtonScreen(video: videoPost))
+          ],
+        );
+      },
+    );
+  }
+}
+/* return PageView(
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
       children: [
@@ -24,4 +43,4 @@ class VideoScrollableScreen extends StatelessWidget {
       ],
     );
   }
-}
+}*/
