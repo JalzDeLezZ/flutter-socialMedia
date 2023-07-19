767d6c5156a33b3fc230e5d8d1bb3f3d46455519
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-07-19 17:19:49 -0500

    [imp] dataSources & repository

---
+++ lib/infrastructure/repository/video_post_repositiry_imp.dart
@@ -0,0 +1,19 @@
+import 'package:toktik/domain/datasource/video_posts_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
+class VideoPostRepositoryImp implements VideoPostRepository {
+  final VideoPostDatasource videoPostDatasource;
+
+  VideoPostRepositoryImp({required this.videoPostDatasource});
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videoPostDatasource.getTrendingVideosByPage(page);
+  }
+}
